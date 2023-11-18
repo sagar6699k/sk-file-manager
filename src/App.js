0c0a0390060e9538c28724c0f8b7503c0a316b68
Sagar <sagarkurewar@gmail.com>
2023-11-18 09:32:56 +0530

    UI Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Homepage from './components/homepage/Homepage';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Welcome to File-Manager</h1>
+      <Homepage/>
     </div>
   );
 }
---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,56 @@
+import "./homepage.css";
+import Form from "react-bootstrap/Form";
+
+const Homepage = () => {
+  let list = [
+    "Natural things1",
+    "Natural things2",
+    "Natural things3",
+    "Natural things4",
+    "Natural things5",
+    "Natural things6",
+    "Natural things7",
+    "Natural things8",
+    "Natural things9",
+  ];
+
+  return (
+    <div className="homepage_container">
+      <div className="input_section">
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Upload the required file</Form.Label>
+          <Form.Control type="file" />
+        </Form.Group>
+      </div>
+      <div className="list_section">
+        {list.map((ele, index) => {
+          return (
+            <div key={index} className="list_item">
+              <div className="list_item_content">
+                <div className="item_image_div">
+                  <img src="" alt="demo" />
+                </div>
+                <div className="item_name_div">{ele}</div>
+              </div>
+              <div className="list_item_action_btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-trash"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                </svg>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
